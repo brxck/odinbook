3d7a588ab05f831ec617cd19f9bda770392dc1c5
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-26 09:40:41 -0700

    Add (unfulfilled) associations to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :friendships
+  has_many :friends, through: :friendships
+
+  has_many :friend_requests
+  has_many :pending_friends, through: :friend_requests
+
+  has_many :posts
+  has_many :comments
+  has_many :likes
+
+  has_one :profile
 end
