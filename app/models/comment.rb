aee4aca60301a1a6e12816ee847a80dda3005f37
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-26 16:52:34 -0700

    Allow comments to have reactions.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
+  has_many :reactions
+
   validates :user, :commentable, :body, presence: true
 end
