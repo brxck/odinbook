edda99fe65dc6fdd4cc86885e718623e5ca88ab9
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-26 20:11:48 -0700

    Generate users controller.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+
   root "pages#home"
 
   get "pages/login", as: "login"
   get "pages/about", as: "about"
   get "pages/contact", as: "contact"
   get "pages/signup", as: "signup"
+
+  get 'users/edit'
+  get 'users/update'
+  get 'users/show'
+  get 'users/index'
+  get 'users/destroy'
 
   resources :friend_requests
   devise_for :users
