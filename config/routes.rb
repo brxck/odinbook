d38e263248e967a54a971114ac4fd78a404a837f
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-26 20:33:22 -0700

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/destroy'
-
   root "pages#home"
 
   get "pages/login", as: "login"
@@ -21,12 +7,9 @@ Rails.application.routes.draw do
   get "pages/contact", as: "contact"
   get "pages/signup", as: "signup"
 
-  get 'users/edit'
-  get 'users/update'
-  get 'users/show'
-  get 'users/index'
-  get 'users/destroy'
-
+  resources :users
+  resources :posts
   resources :friend_requests
+
   devise_for :users
 end
