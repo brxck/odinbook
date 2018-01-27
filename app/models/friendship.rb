42f0340171a292c3e58cd4a6a6bfb8db055ccfc3
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-27 09:24:44 -0700

    Fix friendship infinite loop.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,12 +1,14 @@
 class Friendship < ApplicationRecord
-  after_create :create_inverse
-  after_destroy :destroy_inverse
-  
+  after_create :create_inverse, unless: :has_inverse?
+  after_destroy :destroy_inverse, if: :has_inverse?
+
   belongs_to :user
   belongs_to :friend, class_name: "User"
 
   validate :not_self
-
+  
+  private
+  
   def create_inverse
     friend.friendships.create(friend: user)
   end
@@ -16,8 +18,10 @@ class Friendship < ApplicationRecord
     friendship.destroy if friendship
   end
 
-  private
-
+  def has_inverse?
+    friend.friendships.exists?(friend: user)
+  end
+  
   def not_self
     errors.add(:friend, "can't be equal to user") if user == friend
   end
