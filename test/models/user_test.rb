5584125a0e915922480d72e50c5629f211a1d7e1
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-28 12:34:57 -0700

    Update user model tests.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create(name: "Appa",
+                        email: "appa@cat.net",
+                        password: "testing",
+                        password_confirmation: "testing")
+
+    @friend = User.create(name: "Tori",
+                          email: "jensen@ackles.net",
+                          password: "testing",
+                          password_confirmation: "testing")
+
+    @user.friends << @friend
+  end
+
+  test "default profile" do
+    assert @user.profile
+  end
+
+  test "friends list" do
+    assert_includes @user.friends, @friend
+  end
 end
