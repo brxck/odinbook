861cd98f5ed10aa4f05fdff01d8c898402ae50c5
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-28 13:22:21 -0700

    Add comments to post view.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  has_many :comments, dependent: :destroy
-  has_many :reactions, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :reactions, as: :reactable, dependent: :destroy
 
   validates :user, :body, presence: true
 end
