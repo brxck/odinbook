cba38638280a7771f8ce8102ba6b5be8a385b47d
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-28 19:16:49 -0700

    Add user show page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user
+  
   def edit
   end
 
@@ -6,11 +8,20 @@ class UsersController < ApplicationController
   end
 
   def show
+    @posts = @user.posts
+    @profile = @user.profile
+    @friends = @user.friends
   end
 
   def index
   end
 
   def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
