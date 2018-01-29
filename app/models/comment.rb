77b891ff2c157c5fabf36b234562703f51ac14d2
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-28 20:42:45 -0700

    Add comment reactions.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,15 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
-  has_many :reactions
+  has_many :reactions, as: :reactable, dependent: :destroy
 
   validates :user, :commentable, :body, presence: true
+
+  def blessings
+    reactions.where(name: "bless")
+  end
+
+  def smitings
+    reactions.where(name: "smite")
+  end
 end
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -2,5 +2,13 @@ class Reaction < ApplicationRecord
   belongs_to :user
   belongs_to :reactable, polymorphic: true
 
-  validates :user, :reactable, :type, presence: true
+  validates :user, :reactable, :name, presence: true
+  
+  before_create :delete_other_reaction
+
+  def delete_other_reaction
+    if (other_reaction = Reaction.where(user_id: user_id, reactable: reactable).first)
+      other_reaction.destroy
+    end
+  end
 end
