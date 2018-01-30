3b4f91af35f44bf385a4ba5ce6c5bafaac171e52
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-30 11:20:23 -0700

    Add documentation.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,4 +1,5 @@
 module CommentsHelper
+  # Adds reaction counter if comments are > 0.
   def comment_reaction_text(comment, name)
     if comment.reaction(name).count.zero?
        name.capitalize
@@ -7,6 +8,7 @@ module CommentsHelper
     end
   end
 
+  # Generates reaction link for comments.
   def comment_reaction_link(comment, name)
     link_to comment_reaction_text(comment, name),
     reactions_path(reaction: { reactable_id: comment.id,
