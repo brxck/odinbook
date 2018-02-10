71106aed42d2da26adbe60adb9188df542daa954
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-02-10 11:18:48 -0700

    Add id to buttons for testing.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,14 +5,14 @@ module UsersHelper
 
     if current_user.friends.find_by(id: friend.id)
       button_to "Unfriend", friend_path(friend.id), method: :delete,
-              class: button_class + " is-outlined is-danger"
+              class: button_class + " is-outlined is-danger", id: "unfriend"
     elsif (friend_request = current_user.friend_requests.find_by(friend_id: friend.id))
       button_to "Cancel Request", friend_request, method: :delete,
-              class: button_class + " is-outlined is-info"
+              class: button_class + " is-outlined is-info", id: "cancel-request"
     else
       button_to "Add Friend",
               friend_requests_path(friend_id: friend.id),
-              class: button_class + " is-info"
+              class: button_class + " is-info", id: "add-friend"
     end
   end
 end
