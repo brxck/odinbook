13b09cf2f743785d29db5eb7d6d9a2bfdd73fa08
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-02-11 08:59:27 -0700

    Add icons to friend buttons.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,24 +1,28 @@
 module UsersHelper
   # Generate contextual button to add/remove friend or friend request.
   def friend_button(friend)
+    # Unfriend
     if current_user.friends.find_by(id: friend.id)
-      link_to "Unfriend", friend_path(friend.id), method: :delete,
+      link_to mi.person_outline.to_s + "Unfriend", friend_path(friend.id), method: :delete,
                 class: "button is-outlined is-info", id: "unfriend"
                 
+    # Cancel Request
     elsif (friend_request = current_user.friend_requests.find_by(friend_id: friend.id))
-      link_to "Cancel Request", friend_request, method: :delete,
+      link_to mi.remove.to_s + "Cancel Request", friend_request, method: :delete,
                 class: "button is-outlined is-info", id: "cancel-request"
 
+    # Accept/Reject Request
     elsif (friend_request = current_user.pending_requests.find_by(user_id: friend.id))
-        link_to("Accept Request", friend_request, method: :patch,
+        link_to(mi.person.to_s + "Accept Request", friend_request, method: :patch,
                   class: "button is-success") +
         link_to("Reject", friend_request, method: :delete,
                     class: "button is-outlined is-success")
 
+    # Add Friend
     else
-      link_to "Add Friend",
-                friend_requests_path(friend_id: friend.id),
-                class: "button is-info level-item", id: "add-friend"
+      link_to (mi.person_add.to_s + "Add Friend"),
+              friend_requests_path(friend_id: friend.id), method: :post,
+              class: "button is-info level-item", id: "add-friend"
     end
   end
 end
