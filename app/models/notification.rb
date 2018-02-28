67a4c1da74cff1bfb24c73de5ed79e3745c6f0f9
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-02-27 17:39:44 -0700

    Disable self-notification.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,11 @@ class Notification < ApplicationRecord
   belongs_to :notifiable, polymorphic: true
 
   validates :user, :notifiable, :body, presence: true
+  validate :not_from_self
+
+  def not_from_self
+    if notifiable.user == user
+      errors.add(:user, "can't send yourself a notification")
+    end
+  end
 end
