f87d42dddfc2e45340c22cfeafe4e679c84ac2b9
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-03-01 00:00:44 -0700

    Remove redundant validation.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,7 +5,6 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
 
-  validates :user_id, uniqueness: { scope: :friend_id }
   validate :not_self
 
   private
