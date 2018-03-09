3d3fe387a1332ec979295f2d8eef75fee07cfe7b
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-03-08 22:38:39 -0700

    Remove post button text.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,16 +2,16 @@ module PostsHelper
   # Adds badge to reaction button if reactions are > 0.
   def reaction_button_count(post, name)
     return if (count = post.reaction(name).count).zero?
-    " · #{count}"
+    " #{count}"
   end
 
   # Generates reaction buttons for post.
   def post_reaction_button(post, name, style)
-    link_to reaction_icon(name).to_s + name.capitalize + reaction_button_count(post, name),
+    link_to reaction_icon(name).to_s + reaction_button_count(post, name),
             reactions_path(reaction: { reactable_id: post.id,
                                        reactable_type: "Post",
                                        name: name }),
             method: :post,
-            class: "button is-rounded is-small is-outlined is-info " + style
+            class: "button is-rounded is-small is-outlined " + style
   end
 end
