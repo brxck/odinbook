46536cfce5e2b31691730fad8b1dd41c473b5385
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-03-08 19:58:06 -0700

    Rearrange post reaction buttons.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,23 +1,17 @@
 module PostsHelper
   # Adds badge to reaction button if reactions are > 0.
-  def reaction_button_class(post, name)
-    button_class = "button is-rounded is-small is-outlined is-info"
-
-    if post.reaction(name).count.zero?
-      button_class
-    else
-      button_class + " badge is-badge-info"
-    end
+  def reaction_button_count(post, name)
+    return if (count = post.reaction(name).count).zero?
+    " · #{count}"
   end
 
   # Generates reaction buttons for post.
   def post_reaction_button(post, name, style)
-    link_to reaction_icon(name).to_s + name.capitalize,
+    link_to reaction_icon(name).to_s + name.capitalize + reaction_button_count(post, name),
             reactions_path(reaction: { reactable_id: post.id,
                                        reactable_type: "Post",
                                        name: name }),
             method: :post,
-            class: "#{reaction_button_class(post, name)} #{style}",
-            "data-badge" => post.reaction(name).count
+            class: "button is-rounded is-small is-outlined is-info " + style
   end
 end
