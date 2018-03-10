a2f3d0996a36322b7c289911d507a83e4be9c9df
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-03-10 14:04:10 -0700

    Add link display to posts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129023405) do
+ActiveRecord::Schema.define(version: 20180310202847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20180129023405) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "link_title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
